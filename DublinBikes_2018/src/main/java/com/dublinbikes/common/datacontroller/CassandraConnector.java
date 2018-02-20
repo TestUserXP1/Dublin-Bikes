76d37c4204ec487e1f743238a87709e284a485c3
clantroopss <32268580+clantroopss@users.noreply.github.com>
2018-02-20 18:29:05 +0000

    DataController to connect cassandra

---
+++ DublinBikes_2018/src/main/java/com/dublinbikes/common/datacontroller/CassandraConnector.java
@@ -0,0 +1,27 @@
+package com.dublinbikes.common.datacontroller;
+
+import com.datastax.driver.core.*;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+
+public class CassandraConnector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final Cluster.Builder clusterBuilder = Cluster.builder()
+			    .addContactPoints(
+			        "54.191.120.32"
+			    )
+			    .withPort(9042)
+			    .withAuthProvider(new PlainTextAuthProvider("cassandra", "cassandra"));
+
+			try (final Cluster cluster = clusterBuilder.build()) {
+			    final Metadata metadata = cluster.getMetadata();
+			    System.out.printf("Connected to cluster: %s\n", metadata.getClusterName());
+
+			    for (final Host host: metadata.getAllHosts()) {
+			        System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n", host.getDatacenter(), host.getAddress(), host.getRack());
+			    }
+			}
+	}
+
+}
